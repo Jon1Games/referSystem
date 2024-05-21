2104b6d7aa27a8bdbb06b748932bf58a14eaf385
Jon1Games <118659471+Jon1Games@users.noreply.github.com>
2024-05-21 11:04:01 +0200

    Add files via upload

---
+++ src/main/java/de/jonas/refersystem/ReferSystem.java
@@ -0,0 +1,17 @@
+package de.jonas.refersystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ReferSystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
